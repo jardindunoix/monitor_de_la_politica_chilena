255751dce7fe355473dd0e807c4aa12ea3a2fd2c
rodrigo <rodrigojaviergd@gmail.com>
2023-12-15 18:24:09 -0300

    stable version

--- app/src/main/java/cl/antoinette/monitor_politico_econmico/use_cases/features/diputados/data/DiputadosWebScrapCallProvider.kt
+++ app/src/main/java/cl/antoinette/monitor_politico_econmico/use_cases/features/diputados/data/DiputadosWebScrapCallProvider.kt
@@ -2,53 +2,16 @@ package cl.antoinette.monitor_politico_econmico.use_cases.features.diputados.dat
 
 import cl.antoinette.monitor_politico_econmico.service.StaticUtils
 import cl.antoinette.monitor_politico_econmico.service.StaticUtils.Companion.BASE_URL_DIP_ACT
-import cl.antoinette.monitor_politico_econmico.service.StaticUtils.Companion.DIPUTADOS_DIP_ACT
-import cl.antoinette.monitor_politico_econmico.service.StaticUtils.Companion.GRID_DIP_ACT
-import cl.antoinette.monitor_politico_econmico.service.StaticUtils.Companion.H4_DIP_ACT
-import cl.antoinette.monitor_politico_econmico.service.StaticUtils.Companion.HREF_DIP_ACT
-import cl.antoinette.monitor_politico_econmico.service.StaticUtils.Companion.IMG_DIP_ACT
-import cl.antoinette.monitor_politico_econmico.service.StaticUtils.Companion.SRC_DIP_ACT
-import cl.antoinette.monitor_politico_econmico.use_cases.features.diputados.domain.objects.DiputadoObject
 import org.jsoup.Jsoup
 import org.jsoup.nodes.Document
-import org.jsoup.select.Elements
-import java.util.stream.Collectors
 import javax.inject.Inject
 
 class DiputadosWebScrapCallProvider @Inject constructor() {
     companion object {
         /*LEGISLATIVO*/
-        fun getDiputadosActuales(): MutableList<DiputadoObject> {
-            val diputadosActualesList = mutableListOf<DiputadoObject>()
+        fun getDiputadosActuales(): Document {
             val url = "${BASE_URL_DIP_ACT}${StaticUtils.END_POINT_DIP_ACT}"
-            val document: Document = Jsoup.connect(url).get()
-            val articleElement: Elements = document.select("article.${GRID_DIP_ACT}")
-            val h4Elements = articleElement.select(H4_DIP_ACT).eachText()
-            val nameList = h4Elements.stream().collect(Collectors.toList())
-            val webpage = articleElement.select("a").eachAttr(HREF_DIP_ACT).stream()
-                .collect(Collectors.toList())
-            val imagesList =
-                articleElement.select(IMG_DIP_ACT).eachAttr(SRC_DIP_ACT)
-                    .stream().collect(Collectors.toList())
-            var countAttr = 0
-            if (webpage.size / 3 == nameList.size) {
-                val oldValueOne = "Sr. "
-                val oldValueTwo = "Sra. "
-                val newValue = ""
-
-                for ((i, f) in imagesList.withIndex()) {
-                    diputadosActualesList.add(
-                        DiputadoObject(
-                            nombre = nameList[i].replace(oldValueOne, newValue)
-                                .replace(oldValueTwo, newValue),
-                            paginaWeb = "${BASE_URL_DIP_ACT}${DIPUTADOS_DIP_ACT}${webpage[countAttr]}",
-                            picture = "${BASE_URL_DIP_ACT}${f}"
-                        )
-                    )
-                    countAttr += 3
-                }
-            }
-            return diputadosActualesList
+            return Jsoup.connect(url).get()
         }
 
     }
--- app/src/main/java/cl/antoinette/monitor_politico_econmico/use_cases/features/diputados/domain/DiputadosManager.kt
+++ app/src/main/java/cl/antoinette/monitor_politico_econmico/use_cases/features/diputados/domain/DiputadosManager.kt
@@ -2,11 +2,15 @@ package cl.antoinette.monitor_politico_econmico.use_cases.features.diputados.dom
 
 import android.util.Log
 import androidx.lifecycle.MutableLiveData
+import cl.antoinette.monitor_politico_econmico.service.StaticUtils
 import cl.antoinette.monitor_politico_econmico.use_cases.features.diputados.data.DiputadosWebScrapCallProvider
 import cl.antoinette.monitor_politico_econmico.use_cases.features.diputados.domain.objects.DiputadoObject
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import org.jsoup.nodes.Document
+import org.jsoup.select.Elements
+import java.util.stream.Collectors
 
 class DiputadosManager {
     var allDiputadosActuales = MutableLiveData<MutableList<DiputadoObject>>(mutableListOf())
@@ -14,16 +18,41 @@ class DiputadosManager {
     init {
         CoroutineScope(Dispatchers.IO).launch {
             try {
-                allDiputadosActuales.postValue(getDiputadosActuales())
-            } catch (e:Exception) {
+                val diputadosActualesList = mutableListOf<DiputadoObject>()
+                val document: Document = DiputadosWebScrapCallProvider.getDiputadosActuales()
+                val articleElement: Elements = document.select("article.${StaticUtils.GRID_DIP_ACT}")
+                val h4Elements = articleElement.select(StaticUtils.H4_DIP_ACT).eachText()
+                val nameList = h4Elements.stream().collect(Collectors.toList())
+                val webpage = articleElement.select("a").eachAttr(StaticUtils.HREF_DIP_ACT).stream()
+                    .collect(Collectors.toList())
+                val imagesList =
+                    articleElement.select(StaticUtils.IMG_DIP_ACT).eachAttr(StaticUtils.SRC_DIP_ACT)
+                        .stream().collect(Collectors.toList())
+                var countAttr = 0
+                if (webpage.size / 3 == nameList.size) {
+                    val oldValueOne = "Sr. "
+                    val oldValueTwo = "Sra. "
+                    val newValue = ""
+
+                    for ((i, f) in imagesList.withIndex()) {
+                        diputadosActualesList.add(
+                            DiputadoObject(
+                                nombre = nameList[i].replace(oldValueOne, newValue)
+                                    .replace(oldValueTwo, newValue),
+                                paginaWeb = "${StaticUtils.BASE_URL_DIP_ACT}${StaticUtils.DIPUTADOS_DIP_ACT}${webpage[countAttr]}",
+                                picture = "${StaticUtils.BASE_URL_DIP_ACT}${f}"
+                            )
+                        )
+                        countAttr += 3
+                    }
+                }
+                allDiputadosActuales.postValue(diputadosActualesList)
+            } catch (e: Exception) {
                 Log.e("ERRORRRRR ---->", e.toString())
             }
         }
     }
 
-    private fun getDiputadosActuales():MutableList<DiputadoObject> {
-        return DiputadosWebScrapCallProvider.getDiputadosActuales()
-    }
 }
 
 
