9563bf7e3ce189aa85a1dd5219a4e3d126893e19
rj <rj@gigioberto.com>
2024-08-29 17:20:44 -0400

    spinner loader xml

--- app/src/main/java/cl/antoinette/monitor_politico_econmico/ui/features/home/HomeFragment.kt
+++ app/src/main/java/cl/antoinette/monitor_politico_econmico/ui/features/home/HomeFragment.kt
@@ -12,29 +12,43 @@ import cl.antoinette.monitor_politico_econmico.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
 
-    private var _binding: FragmentHomeBinding? = null
-    private val binding get() = _binding!!
-    private lateinit var navController: NavController
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        _binding = FragmentHomeBinding.inflate(layoutInflater)
-        return binding.root
-    }
+   private var _binding: FragmentHomeBinding? = null
+   private val binding get() = _binding!!
+   private lateinit var navController: NavController
+   override fun onCreateView(
+      inflater: LayoutInflater,
+      container: ViewGroup?,
+      savedInstanceState: Bundle?
+   ): View {
+      _binding = FragmentHomeBinding.inflate(layoutInflater)
+      return binding.root
+   }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) = with(binding) {
-        super.onViewCreated(view, savedInstanceState)
-        navController = Navigation.findNavController(view)
+   override fun onViewCreated(
+      view: View,
+      savedInstanceState: Bundle?
+   ) = with(binding) {
+      super.onViewCreated(view, savedInstanceState)
+      navController = Navigation.findNavController(view)
 
-        buttonIrADipActuales.setOnClickListener {
-            navController.navigate(R.id.action_homeFragment_to_diputadosFragment)
-        }
+      buttonIrADipActuales.setOnClickListener {
+         navController.navigate(R.id.action_homeFragment_to_diputadosFragment)
+      }
 
-    }
+      buttonRecall.setOnClickListener {
+         if (progressbar.visibility == View.GONE) {
+            progressbar.visibility = View.VISIBLE
+            badge.visibility = View.GONE
+         }else{
+            progressbar.visibility = View.GONE
+            badge.visibility = View.VISIBLE
+         }
+      }
 
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
-    }
+   }
+
+   override fun onDestroyView() {
+      super.onDestroyView()
+      _binding = null
+   }
 }
